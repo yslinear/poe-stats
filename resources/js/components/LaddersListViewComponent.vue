a912abee9bec745fedc01e35b1b8a4d266dac491
yslinear <yslinear@gmail.com>
2021-03-03 22:37:17 +0800

    feat: :lipstick: 以 `Vuetify` 重寫聯盟選擇按鈕

--- resources/js/components/LaddersListViewComponent.vue
+++ resources/js/components/LaddersListViewComponent.vue
@@ -2,15 +2,14 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <button
-                    type="button"
-                    class="btn btn-dark mr-1 mb-3 hover:shadow"
-                    @click="updateLadders(league.id)"
-                    v-for="(league, index) in leagues"
-                    :key="index"
-                >
-                    {{ league.id }}
-                </button>
+                <v-select
+                    v-model="select.league"
+                    :items="leagues"
+                    item-text="id"
+                    item-value="id"
+                    label="League"
+                    single-line
+                ></v-select>
             </div>
             <div class="col-12 overflow-scroll">
                 <table class="table">
@@ -82,6 +81,9 @@ export default {
         return {
             leagues: [],
             ladders: [],
+            select: {
+                league: null,
+            },
         };
     },
     mounted() {
@@ -89,13 +91,16 @@ export default {
             this.leagues = res.data;
         });
     },
-    methods: {
-        updateLadders(league) {
-            console.log(league);
-            axios.get(`/api/v1/poe/ladders/${league}`).then((res) => {
-                this.ladders = res.data;
-            });
+    computed: {},
+    watch: {
+        "select.league": function () {
+            axios
+                .get(`/api/v1/poe/ladders/${this.select.league}`)
+                .then((res) => {
+                    this.ladders = res.data;
+                });
         },
     },
+    methods: {},
 };
 </script>
