354e60b407de66733bd643984f0fd1290caa91f8
yslinear <yslinear@gmail.com>
2021-03-07 19:26:01 +0800

    build: :rocket: 在運行 `npm run prod` 時, 在 JS/CSS 檔案連結加入版本號碼

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,4 +16,8 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .postCss("resources/css/app.css", "public/css", [
         require("tailwindcss"),
-    ]);;
+    ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
