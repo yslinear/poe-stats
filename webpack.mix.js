3e07ae84018ed0235eee192ab75a53ea5b1cf3a6
yslinear <yslinear@gmail.com>
2021-03-07 21:03:39 +0800

    build: :hammer: 取消 `laravel-mix` 編譯時的系統提示

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,8 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .postCss("resources/css/app.css", "public/css", [
         require("tailwindcss"),
-    ]);
+    ])
+    .disableNotifications();
 
 if (mix.inProduction()) {
     mix.version();
