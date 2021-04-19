d72eb777a8aa19a221900ad0374d77727339e1bf
yslinear <yslinear@gmail.com>
2021-04-19 12:10:49 +0800

    feat: :lipstick: 修改 404 頁面版面

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8 text-center">
+                <p class="text-6xl">404</p>
+                <p class="text-4xl">oops! Page not found</p>
+            </div>
+        </div>
+    </div>
+@endsection
